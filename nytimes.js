43de48fa855b5d8b16993161f4b8ee5ead725010
Anthony Dell'Agli <anthony.dellagli@uconn.edu>
2016-12-03 11:45:38 -0500

    List NYT reviews and link

--- nytimes.js
+++ nytimes.js
@@ -15,12 +15,22 @@ var NYTSearch = function(searchQuery){
 	  if(result.results.num_results)
 	  	NewYorkTimesReviews = result.results;
 	    let picks = 0;
+	    let reviews = document.getElementById("nytimes-reviews");
 	    for(i=0;i<result.results.length;i++){
 	    	picks += result.results[i].critics_pick;
+	    	let review = result.results[i];
+	    	reviews.innerHTML += "<p>" + 
+	    		"<b>Headline: </b> " + review.headline + "<br>"+
+	    		"<b>Author: </b> " + review.byline + "<br>" +
+	    		"<b>Published: </b> " + review.publication_date + "<br>"+
+	    		"<b>Summary: </b> " + review.summary_short + "<br>"+
+	    		"<a href='" +review.link.url + "'><b>Read More </b> </a>"+
+	    		"</p>";
+
+
 	    }
 	  	document.getElementById('nyTimes').innerHTML =
 	  		"<b>New York Times Critic's Picks:</b> " + picks + "/" + result.results.length;
-
 	}).fail(function(err) {
 	  throw err;
 	});
