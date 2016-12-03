a62f0819f91d76d028fe57476800099ab0613469
Anthony Dell'Agli <anthony.dellagli@uconn.edu>
2016-12-03 10:42:31 -0500

    NYT reviews

---
+++ nytimes.js
@@ -0,0 +1,24 @@
+var NewYorkTimesReviews = []
+
+var NYTSearch = function(searchQuery){
+
+	movieSearch = "Star Wars"
+	var nytUrl = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
+	nytUrl += '?' + $.param({
+	  'api-key': "64483963a3d84321b85ce91504cd34f2",
+	  'query' : searchQuery
+	});
+	$.ajax({
+	  url: nytUrl,
+	  method: 'GET',
+	}).done(function(result) {
+	  console.log(result);
+	  if(result.results.num_results)
+	  	NewYorkTimesReviews = result.results
+
+
+	}).fail(function(err) {
+	  throw err;
+	});
+
+}
